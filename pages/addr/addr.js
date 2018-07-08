473566b07d4d0c57240c22be822add6e90a96352
787701730@qq.com <787701730@qq.com>
2018-07-08 20:15:38 +0800

    新增数据

--- pages/addr/addr.js
+++ pages/addr/addr.js
@@ -1,66 +1,163 @@
 // pages/addr.js
+
+var bmap = require('../../lib/bmap-wx.min.js');
+var {
+  citys,
+  ak
+} = require('../../utils/config.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    citys: [],
+    toView: 'inToView01'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      citys: citys
+    })
   },
+  getLL: function (addr) {
+    wx.request({
+      url: 'https://api.map.baidu.com/geocoder/v2/?address=' + addr + '&output=json&ak=' + ak,
+      data: {},
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
 
+        if (res.statusCode == 200) {
+          // console.log(res.data.result.location);
+          var loc = res.data.result.location;
+          // lng 经度 lat 纬度
+          if (wx.getStorageSync('history')) {
+            var history = wx.getStorageSync('history');
+            var obj = {
+              name: addr,
+              lng: loc.lng,
+              lat: loc.lat
+            };
+            var arr = [obj];
+            var count = 0;
+            for (let i = 0; i < history.length; i++) {
+              if (history[i].name !== arr[0].name) {
+                arr.push(history[i]);
+                count++;
+              }
+              if (count === 9) {
+                break;
+              }
+            }
+            wx.setStorageSync('history', arr);
+          } else {
+            var obj = {
+              name: addr,
+              lng: loc.lng,
+              lat: loc.lat
+            };
+            wx.setStorageSync('history', [obj]);
+          }
+          wx.setStorageSync('queryFlag', 'addr');
+          wx.switchTab({
+            url: '../../pages/index/index'
+          });
+        }
+      }
+    })
+  },
+  scrollToViewFn: function (e) {
+    var _id = e.target.dataset.id;
+    this.setData({
+      toView: 'inToView' + _id
+    })
+    console.log(this.data.toView)
+  },
+  fetchCity: function (e) {
+    // 筛选city
+    var val = e.detail.value;
+    if (val == '') {
+      this.setData({
+        citys: citys
+      })
+    } else {
+      var arr = [];
+      for (let i = 0, len = citys.length; i < len; i++) {
+        if (val.toUpperCase() === citys[i].py) {
+          arr.push(citys[i]);
+        } else {
+          var obj = {
+            py: citys[i].py,
+            data: []
+          };
+          for (let j = 0; j < citys[i].data.length; j++) {
+            if (val.toLowerCase() == citys[i].data[j].pinyin.toLowerCase() || citys[i].data[j].name.indexOf(val) > -1) {
+              obj.data.push(citys[i].data[j]);
+            }
+          }
+          if (obj.data.length) {
+            arr.push(obj);
+          }
+        }
+      }
+      this.setData({
+        citys: arr
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
-  },
 
+  },
+  selectCity: function (e) {
+    this.getLL(e.target.dataset.name);
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,33 +4,97 @@ const app = getApp()
 
 // 引用百度地图微信小程序JSAPI模块 
 var bmap = require('../../lib/bmap-wx.min.js');
+var {
+  ak
+} = require('../../utils/config.js');
+
 Page({
   data: {
-    weatherData: ''
+    currentWeather: '',
+    originalData: '',
+    prompt: ''
   },
   onLoad: function () {
+    wx.setStorageSync('queryFlag', 'fav');
+
+  },
+  onShow: function () {
     var that = this;
     // 新建百度地图对象 
     var BMap = new bmap.BMapWX({
-      ak: 'qCghtLrNGATzBxoIvPxQg0P8hTb01WR5'
+      ak: ak
     });
     var fail = function (data) {
       console.log(data)
     };
     var success = function (data) {
-      var weatherData = data.currentWeather[0];
-      weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' + '日期：' +
-        weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' + '天气：' + weatherData.weatherDesc + '\n' +
-        '风力：' + weatherData.wind + '\n';
       console.log(data);
       that.setData({
-        weatherData: weatherData
+        currentWeather: data.currentWeather[0],
+        prompt: data.originalData.results[0].index,
+        originalData: data.originalData.results[0].weather_data
       });
     }
     // 发起weather请求 
+    let location = '119.3,26.08'; // 默认是福州
+    if (wx.getStorageSync('queryFlag') === 'fav') {
+      //  收藏页面
+      let select = wx.getStorageSync('select');
+      if (select) {
+        location = select.lng + ',' + select.lat;
+      }
+    } else if (wx.getStorageSync('queryFlag') === 'addr') {
+      // 地址页面
+      var history = wx.getStorageSync('history');
+      location = history[0].lng + ',' + history[0].lat;
+    }
+
+
+
     BMap.weather({
       fail: fail,
-      location: '119.3,26.08',
+      location: location,
+      success: success
+    });
+  },
+  onPullDownRefresh: function () {
+    this.setData({
+      citys: []
+    });
+
+
+    var that = this;
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({
+      ak: ak
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      that.setData({
+        currentWeather: data.currentWeather[0],
+        prompt: data.originalData.results[0].index,
+        originalData: data.originalData.results[0].weather_data
+      });
+      wx.stopPullDownRefresh();
+    }
+    // 发起weather请求 
+    let location = '119.3,26.08'; // 默认是福州
+    if (wx.getStorageSync('queryFlag') === 'fav') {
+      //  收藏页面
+      let select = wx.getStorageSync('select');
+      if (select) {
+        location = select.lng + ',' + select.lat;
+      }
+    } else if (wx.getStorageSync('queryFlag') === 'addr') {
+      // 地址页面
+      var history = wx.getStorageSync('history');
+      location = history[0].lng + ',' + history[0].lat;
+    }
+    BMap.weather({
+      fail: fail,
+      location: location,
       success: success
     });
   }
